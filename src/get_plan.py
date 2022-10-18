9c9449bc2cd8e1652305cc6e4eda8b7b07bb0c48
Bing Han@hit <863178540@qq.com>
2022-10-18 14:26:56 +0800

    hello world

---
+++ src/get_plan.py
@@ -0,0 +1,23 @@
+import psycopg2
+
+TIME_OUT = 60000
+
+
+def get_query_plan(query, save_path):
+    conn = psycopg2.connect(dbname="postgres", user="postgres", host="127.0.0.1")
+
+    cur = conn.cursor()
+    cur.execute("SET statement_timeout = {};".format(TIME_OUT))
+    cur.execute("LOAD 'pg_hint_plan';")
+    try:
+        cur.execute(query)
+        result = cur.fetchall()
+        plan = "\n".join(each[0] for each in result)
+
+    except psycopg2.Error as e:
+        plan = e.pgerror
+
+    with open(save_path, "w") as f:
+        f.writelines(plan)
+
+    return plan
